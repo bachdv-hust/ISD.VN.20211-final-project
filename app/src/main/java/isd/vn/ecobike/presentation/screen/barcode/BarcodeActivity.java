3c849095811b0cbc2711a5535ad23be97bf3d5e2
bachdv <bachdv.hust@gmail.com>
2022-01-03 20:54:38 +0700

    change

---
+++ app/src/main/java/isd/vn/ecobike/presentation/screen/barcode/BarcodeActivity.java
@@ -0,0 +1,16 @@
+package isd.vn.ecobike.presentation.screen.barcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import isd.vn.ecobike.R;
+
+public class BarcodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode);
+    }
+}
--- app/src/main/java/isd/vn/ecobike/presentation/screen/main/MainActivity.java
+++ app/src/main/java/isd/vn/ecobike/presentation/screen/main/MainActivity.java
@@ -3,14 +3,28 @@ package isd.vn.ecobike.presentation.screen.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import isd.vn.ecobike.R;
+import isd.vn.ecobike.databinding.ActivityMainBinding;
+import isd.vn.ecobike.presentation.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    protected ActivityMainBinding mBinding ;
+    @Override
+    public View createViewBinding() {
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void afterCreateView() {
+
     }
 }
